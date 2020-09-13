efe353a7710c4df680f7051c6efda1a09f8a8eca
dharmadeveloper108 <whateverland108@gmail.com>
2020-09-13 21:20:21 +0200

    check if on social media for more than 5 min

--- chrome/anti-distraction-tool/background.js
+++ chrome/anti-distraction-tool/background.js
@@ -1,23 +1,37 @@
 // 1. get active tab url && check if tab url matches regex (is social media)
+let timeout;
+
 chrome.tabs.onActivated.addListener( (tabId, changeInfo, tab) => {
     chrome.tabs.query({'active': true, 'currentWindow': true, 'lastFocusedWindow': true}, (tabs) => {
         console.log("Active tab: " + tabs[0].url)
-        if(/\binstagram.com|\byoutube.com|\btwitter.com|\bfacebook.com/g.test( tabs[0].url )) {
-                alert("hi there");
-        }
+// 2. if tab url matches regex, is social media -> start stopwatch
+        triggerAfter10minutes(tabs[0]);
     });
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.active && changeInfo.url) {
-        console.log("Active tab: " + changeInfo.url);    
-        if(/\binstagram.com|\byoutube.com|\btwitter.com|\bfacebook.com/g.test(changeInfo.url)) {
-            alert("hi there");
-    }       
+        console.log("Active tab: " + changeInfo.url);  
+// 2. if tab url matches regex, is social media -> start stopwatch
+        triggerAfter10minutes(changeInfo);
     }
 });
 
-// 2. if tab url matches regex, is social media -> start stopwatch
+
+
+function triggerAfter10minutes(tab) {
+  if(/\binstagram.com|\byoutube.com|\btwitter.com|\bfacebook.com/g.test(tab.url)) {
+    timeout = setTimeout(replacePlaylist, 50000); //5 minutes
+  } else {
+    clearTimeout(timeout);
+  }
+}
+
+function replacePlaylist() {
+
+}
+
+
 
 // if stopwatch time > 10 minutes, get user playlists -> GET https://api.spotify.com/v1/me/playlists
 
